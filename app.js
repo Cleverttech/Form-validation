87f9dfa6b3181925601cbb3ccd5e940208126939
Cleverttech <dzifatedeku@gmail.com>
2021-03-10 14:37:34 +0100

    create email test function for validation

--- app.js
+++ app.js
@@ -16,29 +16,51 @@ function checkInputs() {
   const passwordValue = password.value.trim();
   const password2Value = password2.value.trim();
 
-  if(usernameValue === ''){
-      //show error by adding error class
-      setErrorFor(username, 'Username cannot be empty')
+  if (usernameValue === "") {
+    //show error by adding error class
+    setErrorFor(username, "Username cannot be empty");
+  } else {
+    //show success msg by adding success class
+    setSuccessFor(username);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be empty");
+  }else if(!isEmail(emailValue)){
+      setErrorFor(email, 'Not a valid Email')
   }else{
-      //show success msg by adding success class
-      setSuccessFor(username)
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be empty");
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (password2Value === "") {
+    setErrorFor(password2, "Password cannot be empty");
+  } else {
+    setSuccessFor(password2);
   }
 }
 
-function setErrorFor(input, message){
-    const formControl = input.parentElement; //.form-control
-    const small = formControl.querySelector('small');
+function setErrorFor(input, message) {
+  const formControl = input.parentElement; //.form-control
+  const small = formControl.querySelector("small");
 
-    //add error msg inside small
-    small.innerText = message;
+  //add error msg inside small
+  small.innerText = message;
 
-    //add error class
-    formControl.className = 'form-control error';
+  //add error class
+  formControl.className = "form-control error";
 }
 
-function setSuccessFor(input){
-    const formControl = input.parentElement; 
-    formControl.className = 'form-control success';
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
 }
 
-
+function isEmail(email){  
+    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
